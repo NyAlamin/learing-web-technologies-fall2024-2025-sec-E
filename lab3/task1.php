55dcffb487a1c6679f43f7660912ac247e04dca0
Alamin <96270237+NyAlamin@users.noreply.github.com>
2024-11-11 13:23:41 +0600

    first

---
+++ lab3/task1.php
@@ -0,0 +1,15 @@
+<?php
+  $length = 10;
+  $width = 5;
+
+  $area = $length*$width;
+  $perimeter= 2*($length*$width);
+
+  echo"The Reactangle length = ".$length."<br>";
+  echo"The Reactangle Width= ".$width."<br>";
+  echo"The area of Reactangle= ".$area."<br>";
+  echo"The Perimeter of Reactangle= ".$area."<br>";
+
+
+
+?>
